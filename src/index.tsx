8d96d1e7d089d2717d92dea6ade849e89e6c3bb2
karnpapon <karnpapon@gmail.com>
2020-08-09 20:54:30 +0700

    fix style & remove rust lib

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,4 @@ import * as ReactDOM from 'react-dom';
 import { App } from './App';
 import './index.css';
 
-import('../pkg/rust_binding_lib').then(rust_module => {
-    // console.log("mod",  rust_module.adding(1,4))
-    console.log("mod",  rust_module)
-})
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
